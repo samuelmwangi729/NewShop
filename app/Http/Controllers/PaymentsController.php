2b4facf3fb65d791988055a2ddda047ca8ba0d78
samuelmwangi <samuelmwangi729@gmail.com>
2020-10-03 12:35:54 +0300

    callackurl fixed

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -55,7 +55,7 @@ class PaymentsController extends Controller
          $PartyA=$paymentNumber;
         $PartyB=$this->BusinessCode;
         $PhoneNumber=$paymentNumber;
-        $CallBackURL='https://abed19af9b47.ngrok.io/api/ConfirmPayment';
+        $CallBackURL='https://xpresskenya.tk/api/ConfirmPayment';
         $AccountReference='123456';
         $TransactionDesc='Being Payment for Comodity Ordered';
         $Remark='Being Payment for Order Id  Comodity Ordered';
